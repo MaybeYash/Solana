e1c0692d0e4c258a2eddf904bd675422fd84c573
Yᴀsʜ <unknownsoumo@gmail.com>
2024-09-15 20:24:41 +0530

    good

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F46E5',
+        secondary: '#D946EF',
+        background: '#1E293B',
+      },
+    },
+  },
+  plugins: [],
+};
