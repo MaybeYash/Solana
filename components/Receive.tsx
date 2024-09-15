b55e00b48175bfd840ab00c7ab843e2b54831025
Yᴀsʜ <unknownsoumo@gmail.com>
2024-09-15 20:17:47 +0530

    good

---
+++ components/Receive.tsx
@@ -0,0 +1,7 @@
+export default function Receive() {
+  return (
+    <button className="w-full bg-primary py-2 px-4 rounded-md shadow hover:bg-opacity-80">
+      Receive SOL
+    </button>
+  );
+}
