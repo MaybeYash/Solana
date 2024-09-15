52990f4e497ebe09e565f0c8b24b5d6dacf3a8d4
Yᴀsʜ <unknownsoumo@gmail.com>
2024-09-15 20:19:21 +0530

    good

---
+++ components/Swap.tsx
@@ -0,0 +1,13 @@
+export default function Swap() {
+  const handleSwap = () => {
+  };
+
+  return (
+    <button
+      onClick={handleSwap}
+      className="w-full bg-yellow-500 py-2 px-4 rounded-md shadow hover:bg-opacity-80"
+    >
+      Swap Tokens
+    </button>
+  );
+}
